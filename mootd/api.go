6aad01b9d2024aafeea9d805e71c2a0948ff5cb4
samcole8 <sam@samcole.me>
2024-09-11 19:43:56 +0100

    Add getTimeData function
    - Added getTimeData function to retrieve current time and the renewal
      time for the day

--- mootd/api.go
+++ mootd/api.go
@@ -5,10 +5,24 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func gen() {
 	exec.Command("/bin/sh", "/usr/local/bin/renew.sh").Run()
+}
+
+func getTimeData() (time.Time, time.Time) {
+	// Get current datetime
+	now := time.Now()
+	// Get renewal datetime
+	renewalTimeStr := os.Getenv("RENEWAL_TIME")
+	renewalTime, _ := time.Parse("15:04:05", renewalTimeStr)
+	todayRenewalTime := time.Date(
+		now.Year(), now.Month(), now.Day(),
+		renewalTime.Hour(), renewalTime.Minute(), renewalTime.Second(), 0, now.Location(),
+	)
+	return now, todayRenewalTime
 }
 
 func check() {
